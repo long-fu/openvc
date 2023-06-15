795033620d92f784febe5ab6c3355eb357c92038
haoshuai <haoshuai9192@gmail.com>
2023-06-15 19:40:20 +0800

    C++环境配置

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void say_hello(){
+    std::cout << "Hello, from display!\n";
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    say_hello();
+    return 0;
+}
